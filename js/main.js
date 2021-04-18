6ba24bf30c4c4e0fee4752cebf9a776664f2a17f
yukthimart <81868896+yukthimart@users.noreply.github.com>
2021-04-18 22:52:20 +0530

    sate

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ function myFunction()
 			x.className = "topnav";
 		}
 	}
-
+//3 images
 function prod1js()
 {
 	document.getElementById("prodimgsmall1").style.opacity="1";
@@ -39,7 +39,122 @@ function prod3js()
 	document.getElementById("prodimg2").style.display="none";
 	document.getElementById("prodimg3").style.display="block";
 }
-	
+
+//2 images
+function prod1js2()
+{
+	document.getElementById("prodimgsmall1").style.opacity="1";
+	document.getElementById("prodimgsmall2").style.opacity="0.7";
+	document.getElementById("prodimg1").style.display="block";
+	document.getElementById("prodimg2").style.display="none";
+}
+
+function prod2js2()
+{
+	document.getElementById("prodimgsmall1").style.opacity="0.7";
+	document.getElementById("prodimgsmall2").style.opacity="1";
+	document.getElementById("prodimg1").style.display="none";
+	document.getElementById("prodimg2").style.display="block";
+}
+
+//4 images
+function prod1js4()
+{
+	document.getElementById("prodimgsmall1").style.opacity="1";
+	document.getElementById("prodimgsmall2").style.opacity="0.7";
+	document.getElementById("prodimgsmall3").style.opacity="0.7";
+	document.getElementById("prodimgsmall4").style.opacity="0.7";
+	document.getElementById("prodimg1").style.display="block";
+	document.getElementById("prodimg2").style.display="none";
+	document.getElementById("prodimg3").style.display="none";
+	document.getElementById("prodimg4").style.display="none";
+}
+
+function prod2js4()
+{
+	document.getElementById("prodimgsmall1").style.opacity="0.7";
+	document.getElementById("prodimgsmall2").style.opacity="1";
+	document.getElementById("prodimgsmall3").style.opacity="0.7";
+	document.getElementById("prodimgsmall4").style.opacity="0.7";
+	document.getElementById("prodimg1").style.display="none";
+	document.getElementById("prodimg2").style.display="block";
+	document.getElementById("prodimg3").style.display="none";
+	document.getElementById("prodimg4").style.display="none";
+}
+
+function prod3js4()
+{
+	document.getElementById("prodimgsmall1").style.opacity="0.7";
+	document.getElementById("prodimgsmall2").style.opacity="0.7";
+	document.getElementById("prodimgsmall3").style.opacity="1";
+	document.getElementById("prodimgsmall4").style.opacity="0.7";
+	document.getElementById("prodimg1").style.display="none";
+	document.getElementById("prodimg2").style.display="none";
+	document.getElementById("prodimg3").style.display="block";
+	document.getElementById("prodimg4").style.display="none";
+}
+
+function prod4js4()
+{
+	document.getElementById("prodimgsmall1").style.opacity="0.7";
+	document.getElementById("prodimgsmall2").style.opacity="0.7";
+	document.getElementById("prodimgsmall3").style.opacity="0.7";
+	document.getElementById("prodimgsmall4").style.opacity="1";
+	document.getElementById("prodimg1").style.display="none";
+	document.getElementById("prodimg2").style.display="none";
+	document.getElementById("prodimg3").style.display="none";
+	document.getElementById("prodimg4").style.display="block";
+}
+
+//shops crafts, fashion, arts, gifts
+
+function craftjs()
+{
+	document.getElementById("crafttext").style.opacity = "1";
+	document.getElementById("fashiontext").style.opacity = "0.6";
+	document.getElementById("arttext").style.opacity = "0.6";
+	document.getElementById("giftstext").style.opacity = "0.6";
+	document.getElementById("craftitems").style.display = "block";
+	document.getElementById("fashionitems").style.display = "none";
+	document.getElementById("artitems").style.display = "none";
+	document.getElementById("giftsitems").style.display = "none";
+
+}
+
+function fashionjs()
+{
+	document.getElementById("crafttext").style.opacity = "0.6";
+	document.getElementById("fashiontext").style.opacity = "1";
+	document.getElementById("arttext").style.opacity = "0.6";
+	document.getElementById("giftstext").style.opacity = "0.6";
+	document.getElementById("craftitems").style.display = "none";
+	document.getElementById("fashionitems").style.display = "block";
+	document.getElementById("artitems").style.display = "none";
+	document.getElementById("giftsitems").style.display = "none";
+}
+function artsjs()
+{
+	document.getElementById("crafttext").style.opacity = "0.6";
+	document.getElementById("fashiontext").style.opacity = "0.6";
+	document.getElementById("arttext").style.opacity = "1";
+	document.getElementById("giftstext").style.opacity = "0.6";
+	document.getElementById("craftitems").style.display = "none";
+	document.getElementById("fashionitems").style.display = "none";
+	document.getElementById("artitems").style.display = "block";
+	document.getElementById("giftsitems").style.display = "none";
+}
+function giftsjs()
+{
+	document.getElementById("crafttext").style.opacity = "0.6";
+	document.getElementById("fashiontext").style.opacity = "0.6";
+	document.getElementById("arttext").style.opacity = "0.6";
+	document.getElementById("giftstext").style.opacity = "1";
+	document.getElementById("craftitems").style.display = "none";
+	document.getElementById("fashionitems").style.display = "none";
+	document.getElementById("artitems").style.display = "none";
+	document.getElementById("giftsitems").style.display = "block";
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
